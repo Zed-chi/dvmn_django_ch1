5655540bf3e4e7b5aa6b58b63763fb632b887af8
alex <zed-chi@mail.ru>
2020-06-05 12:18:41 +0300

    added place model

--- afisha/places/models.py
+++ afisha/places/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
-print("test places\n")
+class Place(models.Model):
+    title = models.TextField()
+    placeId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    detailsUrl = models.URLField(max_length=250)
+
+    def __str__(self):
+        """
+        String for representing the MyModelName object (in Admin site etc.)
+        """
+        return self.title
