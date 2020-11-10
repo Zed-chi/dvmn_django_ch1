4374a5f3bcde689bc01512f3a3b688c7453e0e27
zed <zed@mail.ru>
2020-11-10 23:16:04 +0300

    added placeImage model

--- afisha/places/models.py
+++ afisha/places/models.py
@@ -9,3 +9,15 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class PlaceImage(models.Model):
+    image = models.ImageField(upload_to="./images")
+    order = models.IntegerField(default=1)
+    place = models.ForeignKey("Place", related_name="images", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.order} {self.place.title}"
+    
+    class Meta:
+        ordering = ["order"]
