0b730e7f3cf6997e6040a21f0837de3be26397d3
zed <zed@mail.ru>
2020-11-11 21:21:39 +0300

    added inline editing feature in place admin model

--- afisha/places/admin.py
+++ afisha/places/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 # Register your models here.
-admin.site.register(Place)
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImageInline]
+
+
+#admin.site.register(Place)
 admin.site.register(PlaceImage)
