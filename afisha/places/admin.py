8cbf929e25cbe34953d36935daca8b1071bfb43d
zed <zed@mail.ru>
2020-11-11 22:14:57 +0300

    added custom preview field in admin

--- afisha/places/admin.py
+++ afisha/places/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from .models import Place, PlaceImage
 
 # Register your models here.
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
+    readonly_fields = ("preview",)
+
+
+    def preview(self, obj):
+        return format_html(
+            mark_safe(
+                '<img src="{url}" width="{width}" height={height} />'.format(
+                    url=obj.image.url,
+                    width="auto",
+                    height=200,
+                )
+            )
+        )
 
 
 @admin.register(Place)
@@ -11,5 +26,17 @@ class PlaceAdmin(admin.ModelAdmin):
     inlines = [PlaceImageInline]
 
 
-#admin.site.register(Place)
-admin.site.register(PlaceImage)
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    readonly_fields = ("preview",)
+
+    def preview(self, obj):
+        return format_html(
+            mark_safe(
+                '<img src="{url}" width="{width}" height={height} />'.format(
+                    url=obj.image.url,
+                    width="auto",
+                    height=200,
+                )
+            )
+        )
