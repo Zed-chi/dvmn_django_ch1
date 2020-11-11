b98d29dc06acb66f52c318a4f4b6d70ed0b90d97
zed <zed@mail.ru>
2020-11-11 18:36:29 +0300

    added func to get places from db

--- afisha/afisha/views.py
+++ afisha/afisha/views.py
@@ -1,9 +1,43 @@
+import json
 from django.http import HttpResponse
 from django.template import loader
+from places.models import Place, PlaceImage
 
+
+
+def get_places_dict():
+    result = {"type": "FeatureCollection","features": []}
+    places = Place.objects.all()
+    for place in places:
+        detailsUrl = {
+            "title": place.title,
+            "imgs": [img.image.url for img in place.images.all()],
+            "description_short": place.description_short,
+            "description_long": place.description_long,
+            "coordinates": {
+                "lat": place.lat,
+                "lng": place.long
+            }
+        }
+        placeInfo = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.long, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": detailsUrl
+            }
+        }
+        result["features"].append(placeInfo)
+    return result
+    
 
 def index(request):
+    places = get_places_dict()
     template = loader.get_template("index.html")
-    context = {}
+    context = {"places":places}
     rendered_page = template.render(context, request)
     return HttpResponse(rendered_page)
