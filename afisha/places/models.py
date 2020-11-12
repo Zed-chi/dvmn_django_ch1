bb7b7a55f607322f6df29af71eb060c1d6688312
zed <zed@mail.ru>
2020-11-12 13:37:58 +0300

    added wswyg field to place model

--- afisha/places/models.py
+++ afisha/places/models.py
@@ -1,26 +1,32 @@
 from django.db import models
+from tinymce.models import HTMLField
+
 
 class Place(models.Model):
     title = models.CharField(max_length=255)
     description_short = models.TextField()
-    description_long = models.TextField()    
+    description_long = HTMLField()
     lat = models.FloatField()
     long = models.FloatField()
 
     def __str__(self):
         return self.title
-    
+
     class Meta:
-        ordering = ["title",]
+        ordering = [
+            "title",
+        ]
 
 
 class PlaceImage(models.Model):
     image = models.ImageField(upload_to="./images")
     order = models.IntegerField(default=1)
-    place = models.ForeignKey("Place", related_name="images", on_delete=models.CASCADE)
+    place = models.ForeignKey(
+        "Place", related_name="images", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return f"{self.order} {self.place.title}"
-    
+
     class Meta:
         ordering = ["order"]
