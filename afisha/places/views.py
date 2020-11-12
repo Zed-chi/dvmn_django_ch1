df960e5d2e6f1fa406db5de96f28a25ca58504bf
zed <zed@mail.ru>
2020-11-12 18:21:04 +0300

    fix

--- afisha/places/views.py
+++ afisha/places/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from places.models import Place
 
-# Create your views here.
+
 def get_place_view(req, id):
     place = get_object_or_404(Place, pk=id)
     return HttpResponse(place.title)
