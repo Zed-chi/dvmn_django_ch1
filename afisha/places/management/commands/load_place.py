625d47fa297075780786307cafe343668ed17c26
zed <zed@mail.ru>
2020-11-13 00:40:15 +0300

    added custom comand to load place with images

---
+++ afisha/places/management/commands/load_place.py
@@ -0,0 +1,40 @@
+import os
+import json
+import requests
+from io import BytesIO
+from django.core.management.base import BaseCommand, CommandError
+from places.models import Place, PlaceImage
+from afisha.settings import BASE_DIR
+
+
+class Command(BaseCommand):
+    help = "Loading bunch of places"
+    LOAD_DIR = os.path.join(BASE_DIR, "load")
+
+    def add_arguments(self, parser):
+        parser.add_argument("place_json_url", type=str)
+
+    def get_dict_from_json(self, url):
+        res = requests.get(url)
+        return json.loads(res.text)
+
+    def handle(self, *args, **options):
+        info = self.get_dict_from_json(options["place_json_url"])
+        place, created = Place.objects.get_or_create(
+            title=info["title"],
+            description_short=info["description_short"],
+            description_long=info["description_long"],
+            lat=info["coordinates"]["lat"],
+            long=info["coordinates"]["lng"],
+        )
+        print(place)
+        place.save()
+        img_urls = info["imgs"]
+        for order, url in enumerate(img_urls):
+            name = url.split("/")[-1]
+            image = PlaceImage()
+            image.image.save(name, BytesIO(requests.get(url).content), save=False)
+            image.order=order + 1
+            image.place=place
+            image.save()
+        self.stdout.write(self.style.SUCCESS("Successfully loaded new places"))
