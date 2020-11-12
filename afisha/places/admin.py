4d1e9f50074ff23a9377b2ea2c8a52cf5ed2c6c0
zed <zed@mail.ru>
2020-11-12 13:27:38 +0300

    added mouse dragging photos feature to place admin view

--- afisha/places/admin.py
+++ afisha/places/admin.py
@@ -1,13 +1,15 @@
+from django.contrib.admin.options import TabularInline
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
+from adminsortable2.admin import SortableAdminMixin, SortableInlineAdminMixin
 from .models import Place, PlaceImage
 
 # Register your models here.
-class PlaceImageInline(admin.TabularInline):
+class PlaceImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = PlaceImage
     readonly_fields = ("preview",)
-
+    extra = 0
 
     def preview(self, obj):
         return format_html(
@@ -20,10 +22,22 @@ class PlaceImageInline(admin.TabularInline):
             )
         )
 
+    class Meta:
+        ordering = [
+            "order",
+        ]
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    inlines = [PlaceImageInline]
+    inlines = [
+        PlaceImageInline,
+    ]
+
+    class Meta:
+        ordering = [
+            "order",
+        ]
 
 
 @admin.register(PlaceImage)
