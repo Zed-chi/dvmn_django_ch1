a7ab1cbe5469e3a4436d061172511aea2b5a52a8
zed <zed@mail.ru>
2020-11-13 02:25:40 +0300

    added exception handlling

--- afisha/places/management/commands/load_place.py
+++ afisha/places/management/commands/load_place.py
@@ -5,6 +5,7 @@ from io import BytesIO
 from django.core.management.base import BaseCommand, CommandError
 from places.models import Place, PlaceImage
 from afisha.settings import BASE_DIR
+from requests import HTTPError
 
 
 class Command(BaseCommand):
@@ -19,22 +20,24 @@ class Command(BaseCommand):
         return json.loads(res.text)
 
     def handle(self, *args, **options):
-        info = self.get_dict_from_json(options["place_json_url"])
-        place, created = Place.objects.get_or_create(
-            title=info["title"],
-            description_short=info["description_short"],
-            description_long=info["description_long"],
-            lat=info["coordinates"]["lat"],
-            long=info["coordinates"]["lng"],
-        )
-        print(place)
-        place.save()
-        img_urls = info["imgs"]
-        for order, url in enumerate(img_urls):
-            name = url.split("/")[-1]
-            image = PlaceImage()
-            image.image.save(name, BytesIO(requests.get(url).content), save=False)
-            image.order=order + 1
-            image.place=place
-            image.save()
-        self.stdout.write(self.style.SUCCESS("Successfully loaded new places"))
+        try:
+            info = self.get_dict_from_json(options["place_json_url"])
+            place, created = Place.objects.get_or_create(
+                title=info["title"],
+                description_short=info["description_short"],
+                description_long=info["description_long"],
+                lat=info["coordinates"]["lat"],
+                long=info["coordinates"]["lng"],
+            )
+            place.save()
+            img_urls = info["imgs"]
+            for order, url in enumerate(img_urls):
+                name = url.split("/")[-1]
+                image = PlaceImage()
+                image.image.save(name, BytesIO(requests.get(url).content), save=False)
+                image.order = order + 1
+                image.place = place
+                image.save()
+            self.stdout.write(self.style.SUCCESS("Successfully loaded new places"))
+        except (HTTPError, ConnectionError, FileExistsError, CommandError) as e:
+            self.stdout.write(self.style.ERROR(e))
