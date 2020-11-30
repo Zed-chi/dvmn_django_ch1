d44a95b70704d266ce7a9d7a4474773c5bcb0219
zed <zed@mail.ru>
2020-11-30 22:44:07 +0300

    moved try catch higher to for loop

--- afisha/places/management/commands/load_place.py
+++ afisha/places/management/commands/load_place.py
@@ -16,24 +16,16 @@ class Command(BaseCommand):
 
     def load_resource_from(self, url):
         response = requests.get(url)
-        response.raise_for_status()        
+        response.raise_for_status()
         return response
 
     def load_image(self, order, url, place):
-        try:
-            name = url.split("/")[-1]            
-            image_content = self.load_resource_from(url).content
-
-            image = PlaceImage.objects.create(
-                order=order, place=place
-            )
-            image.image.save(name, BytesIO(image_content), save=False)            
-            self.stdout.write(self.style.NOTICE(f"Image {name} saved"))
-        except (
-            requests.HTTPError,
-            FileExistsError,
-        ) as e:
-            self.stdout.write(self.style.ERROR(e))
+        name = url.split("/")[-1]
+        image_content = self.load_resource_from(url).content
+        
+        image = PlaceImage.objects.create(order=order, place=place)
+        image.image.save(name, BytesIO(image_content), save=False)
+        self.stdout.write(self.style.NOTICE(f"Image {name} saved"))
 
     def handle(self, *args, **options):
         info = self.load_resource_from(options["place_json_url"]).json()
@@ -49,5 +41,12 @@ class Command(BaseCommand):
         self.stdout.write(self.style.NOTICE(f"Place {place.title} saved"))
         img_urls = info["imgs"]
         for order, url in enumerate(img_urls, start=1):
-            self.load_image(order, url, place)
+            try:
+                self.load_image(order, url, place)
+            except (
+                requests.HTTPError,
+                FileExistsError,
+            ) as e:
+                self.stdout.write(self.style.ERROR(e))
+
         self.stdout.write(self.style.SUCCESS("Successfully loaded new places"))
